091e4857d600d6fe535f3553dd36984ba671f880
Kostiantyn Kisil <kostikkisil@gmail.com>
2019-12-18 00:13:54 +0200

    avoid rerender with same values

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import {useRef, useState, useLayoutEffect, useCallback} from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
 export interface ElementSize {
@@ -7,26 +7,32 @@ export interface ElementSize {
 }
 
 export const useSize = <T extends HTMLElement>() => {
+  const [{width, height}, setSize] = useState<ElementSize>({width: 0, height: 0});
   const ref = useRef<T | null>(null);
-  const [{width, height}, setSize] = useState<ElementSize>({width: 0, height: 0})
 
-  const observer = new ResizeObserver((entries) => {
-    const {width: newWidth, height: newHeight} = entries[0].contentRect;
+  const onResize = useCallback(() => {
+    if (!ref.current) {
+      return;
+    }
+    const {offsetWidth: newWidth, offsetHeight: newHeight} = ref.current;
 
     if (newWidth !== width || newHeight !== height) {
       setSize({width: newWidth, height: newHeight});
     }
-  })
+  }, [width, height]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    if (!ref || !ref.current) {
+      return;
+    }
+
+    const observer = new ResizeObserver(onResize);
     if (ref && ref.current) {
       observer.observe(ref.current);
 
-      return () => {
-        observer.disconnect();
-      }
+      return () => observer.disconnect();
     }
-  }, [width, height]);
+  }, [ref, onResize]);
 
   return {ref, size: {width, height}};
-}
+};
